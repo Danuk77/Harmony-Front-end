3fc8336e7d8a2b01f31711f23652a4d588582085
tpeach90 <thomasopeach@btinternet.com>
2025-03-13 18:25:02 +0000

    friend block ui improvements

--- Harmony/src/renderer/src/components/FriendBlock.svelte
+++ Harmony/src/renderer/src/components/FriendBlock.svelte
@@ -4,7 +4,6 @@
   import { peerConnectionStatusToBulbColorCssVariable } from '../misc/misc'
   import type { FriendState } from '../../../common/redux'
   import { store } from '../redux'
-  import HarmonyIcon from './HarmonyIcon.svelte'
   import { changeFriendStatus } from '../endpointIoWrappers'
   let {
     selected,
@@ -20,11 +19,7 @@
     oncontextmenu?: HTMLButtonElement['oncontextmenu']
   } = $props()
 
-  let backgroundColor = $derived.by(() => {
-    const lighten =
-      selected && ($store.ui.screenMode == 'chat' || $store.ui.screenMode == 'edit-friend')
-    const amber = hasUnreadMessages
-
+  const getButtonBackgroundColor = (amber: boolean, lighten: boolean) => {
     if (amber) {
       if (lighten) {
         return '--color-block-highlighted-selected'
@@ -38,6 +33,20 @@
         return '--color-block'
       }
     }
+  }
+
+  let statusAndNameBackgroundColor = $derived.by(() => {
+    const lighten = selected && $store.ui.screenMode == 'chat'
+    const amber = hasUnreadMessages
+
+    return getButtonBackgroundColor(amber, lighten)
+  })
+
+  let editBackgroundColor = $derived.by(() => {
+    const lighten = selected && $store.ui.screenMode == 'edit-friend'
+    const amber = hasUnreadMessages
+
+    return getButtonBackgroundColor(amber, lighten)
   })
 
   let bulbColor = $derived.by(() => {
@@ -70,77 +79,106 @@
         return 'fa-envelope'
     }
   })
-
-  const onkeyup: HTMLAnchorElement['onkeyup'] = (event) => {
-    if (event.key == ' ' || event.key == 'Enter') {
-      onclick?.()
-    }
-  }
 </script>
 
-<a href={undefined} id="button" tabindex="0" {onclick} {onkeyup} {oncontextmenu}>
-  <div id="block" style={`background-color: var(${backgroundColor})`}>
+<div id="block">
+  <button
+    id="status-and-name"
+    style={`background-color: var(${statusAndNameBackgroundColor})`}
+    {onclick}
+    {oncontextmenu}
+    aria-label={fs.friend.nickname}
+  >
     {#key fs}
-      <span title={fs.connectionStatus}>
+      <span title={fs.connectionStatus} id="status-span">
         <i class="fas {icon}" id="bulb" style={`color: var(${bulbColor})`}></i>
       </span>
     {/key}
     <p id="nickname">{fs.friend.nickname}</p>
-    <div id="cog-container">
-      <HarmonyIcon
-        icon="fa-cog"
-        ariaLabel="Edit"
-        onclick={(e) => {
-          e.stopPropagation()
-          changeFriendStatus(fs.friend, 'edit')
-        }}
-      />
-    </div>
-  </div>
-</a>
+  </button>
+  <button
+    id="cog-container"
+    style={`background-color: var(${editBackgroundColor})`}
+    onclick={() => changeFriendStatus(fs.friend, 'edit')}
+    aria-label="Edit"
+    {oncontextmenu}
+  >
+    <i class="fas fa-cog fa-lg" id="cog"></i>
+  </button>
+</div>
 
 <style>
-  #cog-container {
-    position: relative;
-    right: 0px;
-    flex-grow: 1;
+  button {
+    all: unset;
+  }
+  button:focus-visible {
+    outline: auto;
+  }
+  #status-and-name {
+    height: 100%;
     display: flex;
     flex-direction: row;
-    justify-content: flex-end;
-    margin-left: 5px;
-    margin-right: 5px;
-  }
-  #button {
-    width: 100%;
-  }
-  #block {
-    background-color: var(--color-block);
-    padding-top: 3px;
-    padding-bottom: 3px;
-
-    border-radius: 5px;
-    margin-top: 1px;
-    margin-bottom: 1px;
-    display: flex;
     align-items: center;
-    /* height: 25px; */
-    max-width: 100%;
+
+    flex-shrink: 1;
+    flex-grow: 1;
+    min-width: 0px;
+    padding-right: 2px;
+    border-radius: 5px 0 0 5px;
   }
-  #block:hover {
+  button:hover {
     opacity: 80%;
     cursor: pointer;
   }
-  #block:active {
+  button:active {
     cursor: pointer;
+    opacity: 60%;
+  }
+  #status-span {
+    flex-shrink: 0;
+    height: max-content;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  #nickname {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    flex-shrink: 1;
+    flex-grow: 1;
+    flex-basis: auto;
+  }
+  #cog-container {
+    /* background-color: var(--color-block); */
+    position: relative;
+    height: 100%;
+    right: 0px;
+    flex-shrink: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+    padding-left: 5px;
+    margin-left: 2px;
+    padding-right: 5px;
+    border-radius: 0 5px 5px 0;
   }
   #bulb {
     margin-left: 5px;
     margin-right: 5px;
     font-size: 20px;
   }
-  #nickname {
-    text-overflow: ellipsis;
-    overflow: hidden;
-    white-space: nowrap;
+
+  #block {
+    margin-top: 1px;
+    margin-bottom: 1px;
+    display: flex;
+    align-items: center;
+    /* height: 25px; */
+    width: 100%;
+    justify-content: space-between;
+    /* overflow: hidden; */
+    height: 30px;
   }
 </style>
